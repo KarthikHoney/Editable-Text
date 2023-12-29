872fabf5650b1209c10598da9bfb4539fa4b8ac6
KarthikHoney <jacksparowkarthick@gmail.com>
2023-12-29 05:59:32 +0000

    toggleFuntion Added

--- src/App.js
+++ src/App.js
@@ -2,37 +2,57 @@ import {Component} from 'react'
 import './App.css'
 
 class App extends Component {
-  state = {inputContent: '', buttonText: 'Save'}
+  state = {inputContent: '', buttonText: 'Save', status: false}
 
   changeInputField = event => {
     this.setState({inputContent: event.target.value})
   }
 
   clickSaveButton = () => {
-    this.setState({buttonText: 'Edit'})
+    this.setState({buttonText: 'Edit', status: true})
+  }
+
+  clickEditButton = () => {
+    this.setState({buttonText: 'Save', status: false})
   }
 
   render() {
-    const {buttonText, inputContent} = this.state
+    const {buttonText, inputContent, status} = this.state
     return (
       <div className="EditableContainer">
         <div className="containerBox">
           <h1 className="editHead">Editable Text Input</h1>
           <div className="inputAndButton">
-            <input
-              type="text"
-              value={inputContent}
-              className="input"
-              placeholder="Enter the Text"
-              onChange={this.changeInputField}
-            />
-            <button
-              type="button"
-              className="button"
-              onClick={this.clickSaveButton}
-            >
-              {buttonText}
-            </button>
+            {!status && (
+              <>
+                <input
+                  type="text"
+                  value={inputContent}
+                  className="input"
+                  placeholder="Enter the Text"
+                  onChange={this.changeInputField}
+                />
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.clickSaveButton}
+                >
+                  {buttonText}
+                </button>
+              </>
+            )}
+            {status && (
+              <>
+                <p className="input">{inputContent}</p>
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.clickEditButton}
+                >
+                  {buttonText}
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
